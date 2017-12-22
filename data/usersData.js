fcb3fa93ce545593ac571f011339b14cadf7b412
Nik Bornemeier <NikolausRB@gmail.com>
2017-12-22 16:21:08 -0700

    refactor seeds, still a WIP

--- data/usersData.js
+++ data/usersData.js
@@ -2,11 +2,15 @@ module.exports = [
   {
     name: 'nik',
     email: 'nik@nik.com',
-    preferredLocation: 'denver, co'
+    preferredLocation: 'denver, co',
+    favBands: ['REM', 'Nathaniel Rateliff and the Nightsweats', 'Jay-Z', 'Cher', 'Metalica'],
+    favVenues: ['Red Rocks', 'The Ogden']
   },
   {
     name: 'ben',
     email: 'ben@ben.com',
-    preferredLocation: 'denver, co'
+    preferredLocation: 'denver, co',
+    favBands: ['STP', 'Beyonce', 'Eminem', 'Laydy Gaga', 'Micheal Bolton'],
+    favVenues: ['Fiddlers Green', 'The Bluebird']
   }
 ];
--- db/seeds/dev/developmentSeed.js
+++ db/seeds/dev/developmentSeed.js
@@ -1,28 +1,38 @@
-// const createVenue = (knex, venue, fan) => {
-//   return knex('users').where('id', fan).first()
-//     .then(venueRecord => {
-//       return knex('favorite_venues').insert({//pretty sure I need to be doing a check here to see if band is already in the table
-//         name: venue.name,
-//         fans: [...fan]
-//       });
-//     });
-// };
-
-// const createBand = (knex, band, fan) => {
-//   return knex('users').where('id', fan).first()
-//     .then(bandRecord => {
-//       return knex('favorite_bands').insert({
-//         name: band.name,
-//         fans: [...fan]
-//       });
-//     });
-// };
-
 const usersInfo = require('../../../data/usersData');
 const bandsInfo = require('../../../data/bandsData');
 const venuesInfo = require('../../../data/venueData');
 const favBands = require('../../../data/bands_usersData');
 const favVenues = require('../../../data/usersVenuesData');
+
+const createBandUserJoin = (knex, band, user) => {
+  console.log('user', user)
+  return knex('users').where('name', user).first()
+    .then(userRecord => {
+      console.log('userRecord', userRecord)
+      return knex('bands').where('bandName', band).first()
+        .then(bandRecord => {
+            console.log(userRecord)
+            console.log(bandRecord)
+          return knex('bands_users').insert({
+            bandId: bandRecord.id,
+            usersId: userRecord.id
+          });
+        });
+    });
+};
+
+const createUserVenueJoin = (knex, venue, user) => {
+  return knex('users').where('name', user).first()
+    .then(userRecord => {
+      return knex('venues').where('venuesName', venue).first()
+        .then(venueRecord => {
+          return knex('bands_users').insert({
+            venueId: venueRecord.id,
+            usersId: userRecord.id
+          });
+        });
+    });
+};
 
 
 
@@ -32,11 +42,32 @@ exports.seed = function (knex, Promise) {
     .then(() => knex('venues').del())
     .then(() => knex('bands').del())
     .then(() => knex('users').del())
-    .then(() => knex('users').insert(usersInfo, 'id'))
     .then(() => knex('bands').insert(bandsInfo, 'id'))
     .then(() => knex('venues').insert(venuesInfo, 'id'))
-    .then(() => knex('bands_users').insert(favBands, 'id'))
-    .then(() => knex('users_venues').insert(favVenues, 'id'))
+    .then(() => {
+      let userObjects = []
+      usersInfo.forEach(user => {
+        userObjects.push({ name: user.name, email: user.email, preferredLocation: user.preferredLocation })
+      })
+      console.log(userObjects)
+      knex('users').insert(userObjects, 'id')
+    })
+    .then( () => {
+      let pendingPromises = [];
+      usersInfo.forEach(user => {
+        let bands = user.favBands;
+        let venues = user.favVenues;
+        bands.forEach(band => {
+          pendingPromises.push(createBandUserJoin(knex, band, user.name));
+        });
+        venues.forEach(venue => {
+          pendingPromises.push(createUserVenueJoin(knex, venue, user.name));
+        });
+
+      });
+      return Promise.all(pendingPromises);
+    })
+
     .then(() => console.log('Dev Seeding Complete!'))
     .catch(error => console.log({ error }));
 };
