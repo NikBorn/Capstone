86a2241cf11a06e66aa70d836dff94337fa799fc
Nik Bornemeier <NikolausRB@gmail.com>
2017-12-28 09:50:09 -0600

    write happy and sad path tests for GET bands:fans

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -155,9 +155,34 @@ describe('API Routes', (done) => {
         .then(response => {
           response.should.have.status(404);
           response.should.be.json;
-          response.should.be.json;
           response.body.should.be.a('object');
           response.body.error.should.equal('No favorite venues saved for user 3');
+        });
+    });
+  });
+
+  describe('GET /api/v1/bands/:id/fans', () => {
+    it('should return an array of bands fans', () => {
+      return chai.request(server)
+        .get('/api/v1/bands/1/fans')
+        .then(response => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('array');
+          response.body.length.should.equal(1);
+          response.body[0].should.have.property('bandId');
+          response.body[0].should.have.property('usersId');
+          response.body[0].should.have.property('id');
+        });
+    });
+    it('should return an error if no fans are following that band', () => {
+      return chai.request(server)
+        .get('/api/v1/bands/1000/fans')
+        .then(response => {
+          response.should.have.status(404);
+          response.should.be.json;
+          response.body.should.be.a('object');
+          response.body.error.should.equal('No fans following to band 1000');
         });
     });
   });
