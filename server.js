91fa91131d857e206fcaec1a625433167ccd7553
Ben Porter <bbp5280@gmail.com>
2018-01-02 16:53:36 -0700

    clean crons a bit more

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static( '../Capstone-Frontend/public/'));
-app.use((request, response, next) => {
+app.use((request, response, next)=>{
   response.header('Access-Control-Allow-Origin', '*');
   next();
 });
