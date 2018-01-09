685554d38e947de34efe137e693fb4abad27bfcf
Nik Bornemeier <NikolausRB@gmail.com>
2018-01-09 07:57:56 -0700

    change port to 3001

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
